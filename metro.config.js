08b01ebb75454fdd3347e9a06e4c03f618f6c2f7
Wali Abdul <waliabdulfatah20@gmail.com>
2026-05-17 03:27:12 -0500

    fix(metro): enable unstable_enablePackageExports for date-fns v4 ESM resolution

--- metro.config.js
+++ metro.config.js
@@ -2,4 +2,7 @@ const { getDefaultConfig } = require('expo/metro-config');
 
 const config = getDefaultConfig(__dirname);
 
+// Required for date-fns v4 (ESM-first package)
+config.resolver.unstable_enablePackageExports = true;
+
 module.exports = config;
